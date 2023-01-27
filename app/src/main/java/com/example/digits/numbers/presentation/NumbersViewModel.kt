c33a5f8d5f72f21703573b536ddfb07c9738ed3f
Foxxx48 <Foxxdim@mail.ru>
2023-01-27 12:07:09 +0300

    added Mapper, Communication, NumbersCommunications, NumbersViewModel

--- app/src/main/java/com/example/digits/numbers/presentation/NumbersViewModel.kt
+++ app/src/main/java/com/example/digits/numbers/presentation/NumbersViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.digits.numbers.presentation
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import com.example.digits.numbers.domain.NumbersInteractor
 
-class NumbersViewModel(
+class NumbersViewModel (
     private val communications: NumbersCommunications,
     private val interactor: NumbersInteractor
-) {
+) : ObserveNumbers  {
     fun fetchFact(s: String) {
 
     }
@@ -16,6 +18,18 @@ class NumbersViewModel(
 
     fun fetchRandomNumberData() {
         TODO("Not yet implemented")
+    }
+
+    override fun observeProgress(owner: LifecycleOwner, observer: Observer<Boolean>) {
+        communications.observeProgress(owner, observer)
+    }
+
+    override fun observeState(owner: LifecycleOwner, observer: Observer<UiState>) {
+       communications.observeState(owner, observer)
+    }
+
+    override fun observeList(owner: LifecycleOwner, observer: Observer<NumberUi>) {
+        communications.observeList(owner, observer)
     }
 
 }
