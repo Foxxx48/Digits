1c540b660cb5f28779b08f547104f07749b3014c
Foxxx48 <Foxxdim@mail.ru>
2023-04-28 17:01:31 +0300

    added Ui tests

---
+++ app/src/androidTest/java/com/example/digits/DetailsPage.kt
@@ -0,0 +1,6 @@
+package com.example.digits
+
+class DetailsPage : Page() {
+
+    val details = R.id.tv_Details.view()
+}
--- app/src/androidTest/java/com/example/digits/NavigationTest.kt
+++ app/src/androidTest/java/com/example/digits/NavigationTest.kt
@@ -1,47 +1,32 @@
 package com.example.digits
 
-import androidx.test.espresso.Espresso.closeSoftKeyboard
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import androidx.test.uiautomator.UiDevice
-import com.example.digits.main.presentations.MainActivity
-import org.junit.Rule
+import androidx.test.espresso.Espresso.pressBack
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-@LargeTest
-class NavigationTest {
-
-    @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
-
+class NavigationTest : BaseTest() {
     @Test
+    fun details_navigation() {
+        val numbersPage = NumbersPage()
+        numbersPage.run {
+            input.typeText("10")
+            getFactButton.click()
 
-    fun detail_navigation(){
-        onView(withId(R.id.et_textfield)).perform(typeText("10"))
-        closeSoftKeyboard()
+            recycler.run {
+                viewInRecycler(0, titleItem).checkText("10")
+                viewInRecycler(0, subTitleItem).checkText("fact about 10")
+                viewInRecycler(0, subTitleItem).click()
+            }
+        }
 
-        onView(withId(R.id.btn_get_fact)).perform(click())
-        onView(withId(R.id.tv_title)).check(matches(withText("10")))
-        onView(withId(R.id.tv_subTitle)).check(matches(withText("fact about 10")))
+        DetailsPage().details.checkText("10\n\nfact about 10")
 
-        onView(withId(R.id.tv_subTitle)).perform(click())
-        onView(withId(R.id.tv_Details)).check(matches(withText("10\n\nfact about 10")))
+        pressBack()
 
-        val device = UiDevice.getInstance(getInstrumentation())
-        device.pressBack()
-        
-        onView(withId(R.id.tv_title)).check(matches(withText("10")))
-        onView(withId(R.id.tv_subTitle)).check(matches(withText("fact about 10")))
+        numbersPage.run {
+            recycler.run {
+                viewInRecycler(0, titleItem).checkText("10")
+                viewInRecycler(0, subTitleItem).checkText("fact about 10")
+            }
+        }
     }
-
 }
